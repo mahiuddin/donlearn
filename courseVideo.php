63a56719566aceb60353ed9278118dbe7c5f3ead
mahiuddin <mahi.ewu@gmail.com>
2020-09-18 14:58:08 +0800

    first commit

---
+++ courseVideo.php
@@ -0,0 +1,340 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <!--====== Required meta tags ======-->
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!--====== Title ======-->
+    <title>Do'n'Learn – </title>
+
+    <!--====== Favicon Icon ======-->
+    <link rel="shortcut icon" href="assets/images/favicon.png" type="image/png">
+
+    <!--====== Bootstrap css ======-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+    <!--====== Animate css ======-->
+    <link rel="stylesheet" href="assets/css/animate.css">
+
+    <!--====== Fontawesome css ======-->
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+    <!--====== Magnific Popup css ======-->
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+
+    <!--====== Nice Select css ======-->
+    <link rel="stylesheet" href="assets/css/nice-select.css">
+
+    <!--====== Slick css ======-->
+    <link rel="stylesheet" href="assets/css/slick.css">
+
+    <!--====== Default css ======-->
+    <link rel="stylesheet" href="assets/css/default.css">
+
+    <!--====== Style css ======-->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!--====== Responsive css ======-->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+
+<style type="text/css">
+
+.video-responsive{
+    overflow:hidden;
+    padding-bottom:56.25%;
+    position:relative;
+    height:0;
+}
+.video-responsive iframe{
+    left:0;
+    top:0;
+    height:100%;
+    width:100%;
+    position:absolute;
+}
+</style>
+
+</head>
+
+<body>
+
+    <!--====== PRELOADER PART START ======-->
+
+    <div id="preloader">
+        <div class="preloader">
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+
+    <!--====== PRELOADER PART ENDS ======-->
+
+    <!--====== Header PART START ======-->
+
+    <section class="header_area">
+        <div class="header_top">
+            <div class="container">
+                <div class="header_top_wrapper d-flex justify-content-center justify-content-md-between">
+                    <div class="header_top_info d-none d-md-block">
+                        <ul>
+                            <li><img src="assets/images/call.png" alt="call"><a href="#">+91 458 654 528</a></li>
+                            <li><img src="assets/images/mail.png" alt="mail"><a href="#">info@example.com</a></li>
+                        </ul>
+                    </div>
+                    <div class="header_top_login">
+                        <ul>
+                            <li><a href="#">Create An Account</a></li>
+                            <li><a class="main-btn" href="#"><i class="fa fa-user-o"></i> Log In</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="header_menu">
+            <div class="container">
+                <nav class="navbar navbar-expand-lg">
+                    <a class="navbar-brand" href="index.html">
+                        <img src="assets/images/logo.png" alt="logo">
+                    </a>
+                    
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="toggler-icon"></span>
+                        <span class="toggler-icon"></span>
+                        <span class="toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
+                        <ul class="navbar-nav m-auto">
+                            <li>
+                                <a href="index.html">Home <i class="fa fa-chevron-down"></i></a>
+                                
+                                <ul class="sub-menu">
+                                    <li><a href="index.html">Home 01</a></li>
+                                    <li><a href="index-2.html">Home 02</a></li>
+                                    <li><a href="index-3.html">Home 03</a></li>
+                                    <li><a href="index-4.html">Home 04</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a class="active" href="about.html">About</a>
+                            </li>
+                            <li>
+                                <a href="courses.html">Courses <i class="fa fa-chevron-down"></i></a>
+                                
+                                <ul class="sub-menu">
+                                    <li><a href="courses.html">Courses</a></li>
+                                    <li><a href="courses-details.html">Courses Details</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="blog.html">Blog <i class="fa fa-chevron-down"></i></a>
+                                
+                                <ul class="sub-menu">
+                                    <li><a href="blog.html">Blog</a></li>
+                                    <li><a href="blog-details.html">Blog Details</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="contact.html">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                    
+                    <div class="navbar_meta">
+                        <ul>
+                            <li>
+                                <a id="search" href="#"><img src="assets/images/search.png" alt="search"></a>
+                                <div class="search_bar">
+                                    <input type="text" placeholder="Search">
+                                    <button><i class="fa fa-search"></i></button>
+                                </div>
+                            </li>
+                            <li><a href="#"><img src="assets/images/cart.png" alt="cart"> <span>0</span></a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </section>
+
+    <!--====== Header PART ENDS ======-->
+
+    <!--====== Page Banner PART START ======-->
+
+    <section class="page_banner bg_cover" style="background-image: url(assets/images/about_bg.jpg)">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="banner_content text-center">
+                        <h4 class="title">State of Matter</h4>
+                        <ul class="breadcrumb justify-content-center">
+                            <li><a href="#">Home</a></li>
+                            <li><a class="active" href="#">State of Matter</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!--====== Page Banner PART ENDS ======-->
+
+    <!--====== About PART START ======-->
+
+    <section class="about_area pt-80">
+        <img class="shap_1" src="assets/images/shape/shape-1.png" alt="shape">
+        <img class="shap_2" src="assets/images/shape/shape-2.png" alt="shape">
+        <img class="shap_3" src="assets/images/shape/shape-3.png" alt="shape">
+        <img class="shap_4" src="assets/images/shape/shape-4.png" alt="shape">
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="about_content mt-45">
+                      
+                        <div class="video-responsive">
+                       <iframe src="https://www.youtube-nocookie.com/embed/MrTxRn9MNWM?showinfo=0&rel=0&cc_load_policy=1&iv_load_policy=3&controls=0&disablekb=1" width="560" height="315" frameborder="0"></iframe>
+                    </div>
+                        
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+
+    <!--====== About PART ENDS ======-->
+
+
+    <!--====== Footer PART START ======-->
+
+    <footer class="footer_area bg_cover" style="background-image: url(assets/images/footer_bg.jpg)">
+        <div class="footer_widget pt-80 pb-130">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="footer_about mt-50">
+                            <a href="#"><img src="assets/images/logo.png" alt="logo"></a>
+                            
+                            <p>Lorem ipsum dolor sit amet dolor, con sect etur adipiscing elitorem ipsum dolorsit amet dolor, con sectetur con sectetur adipisci adipiscing.</p>
+                            
+                            <ul class="footer_social">
+                                <li><a href="#"><i class="fa fa-facebook-f"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                                <li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-9">
+                        <div class="footer_widget_wrapper d-flex flex-wrap">
+                            <div class="footer_link mt-50">
+                                <h5 class="footer_title">Quick Links</h5>
+
+                                <div class="footer_link_wrapper d-flex">
+                                    <ul class="link">
+                                        <li><a href="#">Home</a></li>
+                                        <li><a href="#">About us</a></li>
+                                        <li><a href="#">Education</a></li>
+                                        <li><a href="#">Our Events</a></li>
+                                        <li><a href="#">Our Packages</a></li>
+                                    </ul>
+                                    <ul class="link">
+                                        <li><a href="#">Our Team</a></li>
+                                        <li><a href="#">Latest News</a></li>
+                                        <li><a href="#">Privacy Policy</a></li>
+                                        <li><a href="#">Terms & Condations</a></li>
+                                        <li><a href="#">Contact Us</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class="footer_contact mt-50">
+                                <h5 class="footer_title"><a href="https://www.theidioms.com/contact/">Contact</a></h5>
+                                
+                                <ul class="contact">
+                                    <li>Location : 27 Division St, New York, NY 1002, USA</li>
+                                    <li>Emal : infoexample@gmail.com</li>
+                                    <li>Phone : +(321)948 754</li>
+                                    <li>Fax:+1-212-98765543</li>
+                                    <li><a href="#">View Location on Google Map</a></li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="footer_copyright">
+            <div class="container">
+                <div class="footer_copyright_wrapper text-center d-md-flex justify-content-between">
+                    <div class="copyright">
+                        <p class="cr">Made with <span class="heart"><i class="fa fa-heart"></i></span> by <a target="_blank" href="https://www.theidioms.com">Idioms</a></p>
+                    </div>
+                    <div class="copyright">
+                        <p>&copy; Copyrights 2022 Edustdy All rights reserved. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!--====== Footer PART ENDS ======-->
+    
+    <!--====== BACK TOP TOP PART START ======-->
+
+    <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+    <!--====== BACK TOP TOP PART ENDS ======-->
+
+    
+
+
+
+
+
+
+
+
+
+    <!--====== jquery js ======-->
+    <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+
+    <!--====== Bootstrap js ======-->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+
+    <!--====== Slick js ======-->
+    <script src="assets/js/slick.min.js"></script>
+
+    <!--====== Magnific Popup js ======-->
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+
+    <!--====== Counter Up js ======-->
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+
+    <!--====== Nice Select js ======-->
+    <script src="assets/js/jquery.nice-select.min.js"></script>
+    
+    <!--====== Count Down js ======-->
+    <script src="assets/js/jquery.countdown.min.js"></script>
+    
+    <!--====== Appear js ======-->
+    <script src="assets/js/jquery.appear.min.js"></script>
+
+    <!--====== Main js ======-->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
